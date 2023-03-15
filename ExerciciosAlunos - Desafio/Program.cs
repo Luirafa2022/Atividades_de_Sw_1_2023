a3e55b1e3bfc5b80ec83a0991255413d2c8bbad0
Luirafa2022 <100379672+Luirafa2022@users.noreply.github.com>
2023-03-15 15:29:18 -0300

    Add files via upload

---
+++ ExerciciosAlunos - Desafio/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ExerciciosAlunos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite seu nome: ");
+        string? nomealuno = Console.ReadLine();
+        Console.WriteLine("O seu nome é " + nomealuno);
+        Console.Write("Digite um número: ");
+        double numero1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("O número digitado foi " + numero1);
+        Console.Write("Digite outro número: ");
+        double numero2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("O número digitado foi " + numero2);
+        Console.WriteLine("A média de " + nomealuno + " foi de: " + (numero1 + numero2)/2);
+    }
+}
