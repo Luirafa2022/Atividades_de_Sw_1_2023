2539fe7ed79e4a67a47b6dba3736fa79628b071c
Luirafa2022 <100379672+Luirafa2022@users.noreply.github.com>
2023-03-15 15:01:38 -0300

    Add files via upload

---
+++ ExerciciosAlunos/Aluno.cs
@@ -0,0 +1,29 @@
+namespace ExerciciosAlunos;
+class Aluno
+{
+    public string nome = "";
+    public double nota1, nota2;
+
+    //Média -> retornar o double (por exemplo um número como 8.3)
+    public double obterMedia(){
+        double media = (nota1 + nota2)/2;
+        return media;
+    }
+    //Situação -> retornar uma string (por exemplo "Aprovado" e "Reprovado")
+    public string obterSituacao(double media){
+        string situacao="";
+        if (media>=6){
+            situacao="aprovado";
+            }else{
+                situacao="reprovado";
+            }
+            return situacao;
+
+    }
+    //Mensagem -> não retorna nada. Só mostra na tela os detalhes (nome, média, situação)
+    public void mensagem(){
+        double mediaCalculada = obterMedia();
+        string resultadoSituacao = obterSituacao(mediaCalculada);
+        Console.WriteLine(nome + " está " + resultadoSituacao + " com média: " + mediaCalculada);
+    }
+}
---
+++ ExerciciosAlunos/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ExerciciosAlunos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno aluno1 = new Aluno();
+        aluno1.nome = "Luiz";
+        aluno1.nota1 = 6.2;
+        aluno1.nota2 = 8.9;
+
+        aluno1.mensagem();
+    }
+}
