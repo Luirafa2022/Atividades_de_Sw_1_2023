27d952ff0529477760be5e8a0833b804e4bc23b0
Luirafa2022 <100379672+Luirafa2022@users.noreply.github.com>
2023-03-15 16:31:32 -0300

    Add files via upload

--- ExerciciosAlunos - Desafio/Program.cs
+++ ExerciciosAlunos - Desafio/Program.cs
@@ -12,6 +12,13 @@ class Program
         Console.Write("Digite outro número: ");
         double numero2 = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("O número digitado foi " + numero2);
-        Console.WriteLine("A média de " + nomealuno + " foi de: " + (numero1 + numero2)/2);
+         string situacao="";
+        if ((numero1 + numero2)/2>=6){
+            situacao="aprovado";
+            }else{
+                situacao="reprovado";
+            }
+        Console.WriteLine("A média de " + nomealuno + " foi de: " + (numero1 + numero2)/2 + " e ele foi " + situacao);
+        
     }
 }
